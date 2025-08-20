0ea60477615b93fd82a3db43bf52a01b9fe748e3
venblaras <carlos847@gmail.com>
2025-08-19 22:01:16 -0300

    build: add Tailwind CSS and Autoprefixer
    - Added Tailwind CSS for styling
    - Added Autoprefixer for vendor prefixes
    - Updated package.json and package-lock.json
    - Created postcss.config.js and tailwind.config.js
    - Moved styles to src/styles folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Sem a propriedade 'base', pois o site estará na raiz do domínio.
   plugins: [react()],
-})
+});
