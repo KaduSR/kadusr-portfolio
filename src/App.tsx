a578b75e5a5fa14dad22561ee768e886d8ec8e28
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-20 14:08:35 -0300

    feat: update app UI and add logo
    - Replaced old components with new ones.
    - Added a header component.
    - Implemented new styling.
    - Added Logo.svg and menuIcon.svg.
    - Improved overall UI design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
+import React from "react";
+import { Header } from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-[#1E1E1E] text-white min-h-screen font-sans">
+      <main className="container mx-auto px-4">
+        <Header/>.
+        <h1 className="text-4xl text-center pt-24">Construção constante do meu Portfólio</h1>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,20 @@
-import { Header } from "@/styles/Header.module.css";
+import React, { useState, useEffect } from "react";
+import * as classes from "../styles/module/Header.module.css";
+
+const navItems = [
+    { label: "Sobre", href: "#about" },
+    { label: "Experiência", href: "#experience" },
+    { label: "Projetos", href: "#projects" },
+    { label: "Contato", href: "#contact" },
+    
+]
+
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    return (
+        
+    );
+};
+
+
