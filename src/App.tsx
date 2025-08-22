dc210c0c607d7fd2bb72aced0b754746c9821b5b
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-22 12:17:27 -0300

    feat(components): add About component
    - Created About component
    - Added expandable text section
    - Implemented state management
    - Used styled-components
    - Integrated profile data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { ProfileCard } from "./components/ProfileCard";
+import { About } from "./components/About";
 
 function App() {
   return (
     <div className="bg-[#1E1E1E] text-white min-h-screen font-sans">
       <main className="container mx-auto px-4">
         <Header />
-        <ProfileCard/>
+        <ProfileCard />
+        <About />
         <h1 className="text-4xl text-center pt-24">Construção constante do meu Portfólio</h1>
       </main>
     </div>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,1 +1,22 @@
-import { About } from "@/styles/About.module.css";
+import styles from "@/styles/module/About.module.css";
+import { profileData } from "../data";
+import React, { useState } from "react";
+
+export const About = () => {
+  // Estado para controlar se o texto está expandido ou não.
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <section id="about" className={styles.aboutSection}>
+      <h2 className="{styles.title}">Um pouco sobre mim: </h2>
+      {/* Lógica condicional: Se 'isExpanded' for true, mostra o texto longo, senão, o curto. */}
+      <p className={styles.text}>
+        {isExpanded ? profileData.about.long : profileData.about.short}
+          </p>
+          <button className="{styles.readMoreButton}" onClick={() => setIsExpanded}
+          >
+              {isExpanded ? "Ler menos" : "Ler mais"}
+</button>
+    </section>
+  );
+};
