f3897887a622fce9ad32c7567bec1cc17dcfe0b5
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-22 18:36:41 -0300

    refactor(styles): remove unnecessary styles
    - Removed unused CSS classes.
    - Simplified App component layout.
    - Improved ProfileCard styling.
    - Updated main container width.
    - Minor CSS cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,11 @@ import { About } from "./components/About";
 function App() {
   return (
     <div className="bg-[#1E1E1E] text-white font-sans">
-      <main className="container mx-auto">
+      <main className="w-full">
         <Header />
-        <div className="pt-23" id="profile-card-container">
+        <div className="pt-23">
           <ProfileCard />
         </div>
-
         <About />
         <h1 className="text-4xl text-center pt-24">
           Construção constante do meu Portfólio
