dfa494d2b076ca6cf326a6437ab89d04e59bd902
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-22 17:27:34 -0300

    refactor(styles): update styling and layout
    - Adjusted App component layout.
    - Updated max-width for #root.
    - Removed unnecessary padding.
    - Refined ProfileCard styling.
    - Improved responsiveness.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { About } from "./components/About";
 
 function App() {
   return (
-    <div className="bg-[#1E1E1E] text-white min-h-screen font-sans">
-      <main className="container mx-auto px-4 w-full">
-          <Header/>
+    <div className="bg-[#1E1E1E] text-white font-sans">
+      <main className="container mx-auto">
+        <Header />
         <div className="pt-23" id="profile-card-container">
           <ProfileCard />
         </div>
